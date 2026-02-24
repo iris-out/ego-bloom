8878643e8ffcc8a3fd35675283f854fcc58fa887
Anonymous <username@users.noreply.github.com>
2026-02-24 16:31:48 +0900

    wow

--- testapi.js
+++
@@ -1,3 +0,0 @@
-fetch('https://api.zeta-ai.io/v1/plots/ranking?type=TRENDING&limit=1&filterType=GENRE&filterValues=all').then(r => r.json()).then(d => {
-    console.log(JSON.stringify(d.rankings?.[0] || d.plots?.[0], null, 2));
-});
